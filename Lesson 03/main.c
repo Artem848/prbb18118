e121dfda1eb7cccce6bacb93b528861fe6d0a203
Nikitin Viktor <nikitin@rubiconepro.ru>
2018-12-06 19:33:22 +0300

    End of lesson 03

---
+++ Lesson 03/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+	//char name[10] = "John Doe";
+	//int age;
+
+	//printf("Hello, %s!\n", name);
+
+	//printf("How old are you?\n");
+	//scanf_s("%d", &age);
+
+	//printf("You look younger, I thought you are %d years old.\n", age - 3);
+
+	int first;
+	int second;
+
+	printf("Enter first number!\n");
+	scanf_s("%d", &first);
+
+	printf("Enter second number!\n");
+	scanf_s("%d", &second);
+
+	printf("Sum of numbers is %d!\n", first + second);
+
+	system("pause"); // C++ function
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # Homeworks
 
-1. Install minGW to your home computer
-1. Install VSCode to your home computer
-1. Install Microsoft code snippets for c/c++, for VSCode
-1. Find out how does gcc work
-1. Compile main.c to yourSurName.exe using gcc
-1. Run this programm with debugger in VSCode
-test
+1. Write programm printing difference of two numbers.
+1. Write programm printing multiplication of two numbers.
+1. Write programm printing division of two numbers.
